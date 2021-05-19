dcb65b526e6455a718cac44e469c8fdbad278340
MadinaTanikulova <83603205+MadinaTanikulova@users.noreply.github.com>
2021-05-19 15:23:42 +0600

    Update and rename Population_increase.java to PopulationIncrease.java

--- Week1/PopulationIncrease.java
+++ Week1/PopulationIncrease.java
@@ -1,17 +1,18 @@
-/*javac -d . *.java
-
-java Population_increase
-
-*/
+/*
+ *javac -d . *.java
+ *
+ *java Population_increase
+ *
+ */
 
 import java.util.Scanner;
 
-public class Population_increase{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        int N = Integer.parseInt(scanner.nextLine());
-        for(int i = 0; i < N; i++){
+public class PopulationIncrease {
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+          int N = Integer.parseInt(scanner.nextLine());
+        
+          for(int i = 0; i < N; i++){
             String[] array = scanner.nextLine().split(" ");
             int PA = Integer.parseInt(array[0]);
             int PB = Integer.parseInt(array[1]);
@@ -20,18 +21,19 @@ public class Population_increase{
 
 
             int count = 0;
-            while(PA <= PB){
-                PA = (int)(PA+PA*(g1 / 100));
+            while (PA <= PB) {
+                PA = (int) (PA + PA * (g1 / 100));
                 PB = (int) (PB + PB * (g2 / 100));
                 count++;
-                if(count > 100)
+                if(count > 100){
                     break;
+                }
             }
-            if(count <= 100)
+            if(count <= 100) {
             System.out.println(count + " anos.");
-            else 
+            }else {
             System.out.println("Mais de 1 seculo.");
-
-        }
+            }
+         }
     }
 }
