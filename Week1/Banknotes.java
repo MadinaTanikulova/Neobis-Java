d2432a1be11fb3d89bdb9a2732668354d3318592
MadinaTanikulova <83603205+MadinaTanikulova@users.noreply.github.com>
2021-05-19 14:46:46 +0600

    Update Banknotes.java

--- Week1/Banknotes.java
+++ Week1/Banknotes.java
@@ -1,8 +1,9 @@
-/*javac -d . *.java
-
-java Banknotes
-
-*/
+/*
+ *javac -d . *.java
+ *
+ *java Banknotes
+ *
+ */
 
 import java.util.Scanner;
 
@@ -10,24 +11,23 @@ public class Banknotes {
    static int N;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-      N = scanner.nextInt();
+        N = scanner.nextInt();
         System.out.println(N);
-        System.out.println(Result(100)+ " nota(s) de R$ 100,00\n" +
-                Result(50) + " nota(s) de R$ 50,00\n" +
-                Result(20) + " nota(s) de R$ 20,00\n" +
-                Result(10)+ " nota(s) de R$ 10,00\n" +
-                Result(5) + " nota(s) de R$ 5,00\n" +
-                Result(2)+ " nota(s) de R$ 2,00\n" +
-                Result(1)+ " nota(s) de R$ 1,00");
-    }
+        System.out.println(Result(100) + " nota(s) de R$ 100,00\n" +
+                           Result(50) + " nota(s) de R$ 50,00\n" +
+                           Result(20) + " nota(s) de R$ 20,00\n" +
+                           Result(10) + " nota(s) de R$ 10,00\n" +
+                           Result(5) + " nota(s) de R$ 5,00\n" +
+                           Result(2) + " nota(s) de R$ 2,00\n" +
+                           Result(1) + " nota(s) de R$ 1,00");
+   }
 
-    public  static int Result(int banknote){
-        int resultofBanknotes= 0;
-        if(N>= banknote) {
+    public static int Result(int banknote){
+        int resultofBanknotes = 0;
+        if (N >= banknote) {
             resultofBanknotes = N / banknote;
-            N%= banknote;
+            N % = banknote;
         }
-        return resultofBanknotes;
-
+        return resultofBanknotes;                                    
     }
 }
