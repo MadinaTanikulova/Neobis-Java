1bbc8c3d352f58ac85bd7eec278c0f8067953224
MadinaTanikulova <83603205+MadinaTanikulova@users.noreply.github.com>
2021-05-19 15:56:37 +0600

    Update Fibonacci.java

--- Week 2/Fibonacci.java
+++ Week 2/Fibonacci.java
@@ -1,22 +1,20 @@
-/*javac -d . *.java
-
-java Fibonacci
-
-*/
-
-
+/*
+ *javac -d . *.java
+ *
+ *java Fibonacci
+ *
+ */
 
 import java.util.Scanner;
 
 public class Fibonacci{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int N = scanner.nextInt();    
         int []array =new int [N];
         for(int i = 0;  i < N; i++) {
              array[i] = scanner.nextInt();
-  System.out.printf("Fib(%d) = %d%n", array[i], Fibo(array[i]));
+              System.out.printf("Fib(%d) = %d%n", array[i], Fibo(array[i]));
 	}
 
     }
@@ -28,7 +26,7 @@ public class Fibonacci{
             long array1 = 0;
             long array2 = 1;
             for (int i = 2; i <= number; i++) {
-                long result =array1 + array2;
+                long result = array1 + array2;
                 array1  = array2;
                 array2 = result;
             }
